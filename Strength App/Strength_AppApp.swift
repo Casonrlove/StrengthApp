3bf8a3dbe569d2a79bf8b94fb05c4fe61a246fa4
Casonrlove <Casonrlove@tamu.edu>
2024-03-22 12:20:06 -0500

    Initial Commit

---
+++ Strength App/Strength_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Strength_AppApp.swift
+//  Strength App
+//
+//  Created by Cason Love on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Strength_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Strength AppUITests/Strength_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Strength_AppUITestsLaunchTests.swift
+//  Strength AppUITests
+//
+//  Created by Cason Love on 3/22/24.
+//
+
+import XCTest
+
+final class Strength_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
