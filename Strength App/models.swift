4131617f01c3224e03aaca109c0434747665aedd
jackson-hamelin <jacksonbenge@gmail.com>
2024-03-29 00:34:35 -0500

    jacksons-branch

--- Strength App/models.swift
+++ Strength App/models.swift
@@ -10,25 +10,16 @@ import Foundation
 //video said struct should have the workouts and we could make a class for the template of workouts?
 
 //Hasher means each workout has an INT value behind the scenes
-struct Workout: Hashable {
-    var exercise: String
-    var weight: Double
-    var reps: Int
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(exercise)
-        hasher.combine(weight)
-        hasher.combine(reps)
-    }
-    
-    static func == (lhs: Workout, rhs: Workout) -> Bool {
-        return lhs.exercise == rhs.exercise && lhs.weight == rhs.weight && lhs.reps == rhs.reps
-    }
+struct Exercise: Hashable{
+    var ExerciseName : String
+    var ExerciseWeight : Double
+    var ExerciseReps : Int
 }
 
-struct WorkoutSession: Identifiable, Hashable {
-    let id = UUID()
-    var date: Date
-    var title: String  // Add title property
-    var workouts: [Workout]
+//thinking we can call the workout class to store workouts?
+// question would be how to list them... wonder if another array could just be made of the names of the list? idk functionality for the future?
+class Workout {
+    static let workout: [Exercise] = []
 }
+
+
